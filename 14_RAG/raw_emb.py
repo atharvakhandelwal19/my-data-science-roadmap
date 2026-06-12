1b619498a27faf2f90742cb53ce406c052c3c7bc
atharvakhandelwal19 <atharvakhandelwal19@gmail.com>
2026-06-12 19:50:03 +0530

    RAG chunks embedding

--- 14_RAG/raw_emb.py
+++ 14_RAG/raw_emb.py
@@ -1,26 +1,34 @@
 from dotenv import load_dotenv
 import os
-
-from langchain_google_genai import ChatGoogleGenerativeAI
+from openai import OpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 
 load_dotenv()
 
-client=ChatGoogleGenerativeAI(
-    api_key=os.getenv("GEMINI_API_KEY")
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
-conversation = client.chat.completions.create(
-    model='gemini-2.5-flash',
-    messages=[
-        {"role":"system", "content":"You are a helpful assistant"},
-        {"role":"user", "content":"What is the capital of France?"}
-    ]
+# conversation = client.chat.completions.create(
+#     model="gemini-2.5-flash",
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "What is the capital of France?"},
+#     ]
+# )
+
+# print(conversation.choices[0].message.content)
+
+
+response = client.embeddings.create(
+    input="This is a good conversation", 
+    model="gemini-embedding-2" 
 )
 
-print(conversation.choices[0].message.content)
 
+embedding_vector = response.data[0].embedding
 
-
-client.embeddings.create(
-    input="This is a good conversation", model=
-)
+print(f"Dimensions: {len(embedding_vector)}")
+print('='*100)
+print(f"First 5 values: {embedding_vector[:5]}")
